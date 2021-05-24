12de06c5308b0532e0b83ed4a093d1ddc9eea75d
Andreas Kahler <mail@andreaskahler.com>
2021-05-24 15:17:47 +0200

    trying to get uart to work

--- winkdings/src/winkdings-bluepill/src/serialout.c
+++ winkdings/src/winkdings-bluepill/src/serialout.c
@@ -13,6 +13,8 @@
 
 #include "stm32f1xx.h"
 #include "stm32f1xx_hal.h"
+#include "bsp/board.h"
+#include "uart.h"
 
 /* UART handler declaration */
 UART_HandleTypeDef UartHandle;
@@ -25,9 +27,29 @@ uint8_t aTxBuffer[] = "\n\r ****UART-Hyperterminal communication based on DMA***
 uint8_t aRxBuffer[RXBUFFERSIZE];
 
 /* Private function prototypes -----------------------------------------------*/
-void SystemClock_Config(void);
-static void Error_Handler(void);
 
+static void Error_Handler(void);
+void HAL_UART_MspInit(UART_HandleTypeDef *huart);
+
+int delay(int millis)
+{
+  uint32_t m = board_millis();
+  int i=0;
+  while (board_millis() < m+millis)
+    i++;
+  return i;
+}
+
+void blink(void)
+{
+  for(int i=0; i<10; ++i)
+  {
+    board_led_write(false);
+    delay(100);
+    board_led_write(true);
+    delay(100);
+  }
+}
 
 void serial_init(void)
 {
@@ -41,7 +63,7 @@ void serial_init(void)
       - Parity      = ODD parity
       - BaudRate    = 9600 baud
       - Hardware flow control disabled (RTS and CTS signals) */
-  UartHandle.Instance          = USART1;
+  UartHandle.Instance          = USARTx;
   
   UartHandle.Init.BaudRate     = 9600;
   UartHandle.Init.WordLength   = UART_WORDLENGTH_8B;
@@ -55,6 +77,7 @@ void serial_init(void)
     /* Initialization Error */
     Error_Handler();
   }
+  HAL_UART_MspInit(&UartHandle);
 }
 
 void serial_send(const char* buffer, int count)
@@ -65,10 +88,11 @@ void serial_send(const char* buffer, int count)
       For simplicity reasons, this example is just waiting till the end of the
       transfer, but application may perform other tasks while transfer operation
       is ongoing. */
-  while (HAL_UART_GetState(&UartHandle) != HAL_UART_STATE_READY)
-  {
-  }
+  //while (HAL_UART_GetState(&UartHandle) != HAL_UART_STATE_READY)
+  //{
+  //}
   
+  blink();
   if (HAL_UART_Transmit_DMA(&UartHandle, (uint8_t *)buffer, count) != HAL_OK)
   {
     /* Transfer error in transmission process */
@@ -105,11 +129,13 @@ void serial_receive(void)
 static void Error_Handler(void)
 {
   /* Toogle LED2 for error */
-  //while(1)
-  //{
-  //  BSP_LED_Toggle(LED2);
-  //  HAL_Delay(1000);
-  //}
+  while(1)
+  {
+    board_led_write(false);
+    delay(100);
+    board_led_write(true);
+    delay(100);
+  }
 }
 
 /**
@@ -139,6 +165,139 @@ void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
   /* Turn LED2 on: Transfer in reception process is correct */
   //BSP_LED_On(LED2);
 }
+
+
+////////////////////////////////////////////////////////////////////////////////////////////
+// from https://github.com/STMicroelectronics/STM32CubeF1/blob/master/Projects/STM32F103RB-Nucleo/Examples/UART/UART_HyperTerminal_DMA/Src/stm32f1xx_hal_msp.c
+////////////////////////////////////////////////////////////////////////////////////////////
+
+/**
+  * @brief UART MSP Initialization
+  *        This function configures the hardware resources used in this example:
+  *           - Peripheral's clock enable
+  *           - Peripheral's GPIO Configuration
+  *           - DMA configuration for transmission request by peripheral
+  *           - NVIC configuration for DMA interrupt request enable
+  * @param huart: UART handle pointer
+  * @retval None
+  */
+void HAL_UART_MspInit(UART_HandleTypeDef *huart)
+{
+  static DMA_HandleTypeDef hdma_tx;
+  static DMA_HandleTypeDef hdma_rx;
+
+  GPIO_InitTypeDef  GPIO_InitStruct;
+
+  /*##-1- Enable peripherals and GPIO Clocks #################################*/
+  /* Enable GPIO clock */
+  USARTx_TX_GPIO_CLK_ENABLE();
+  USARTx_RX_GPIO_CLK_ENABLE();
+
+
+
+  /* Enable USARTx clock */
+  USARTx_CLK_ENABLE();
+
+  /* Enable DMA clock */
+  DMAx_CLK_ENABLE();
+
+  /*##-2- Configure peripheral GPIO ##########################################*/
+  /* UART TX GPIO pin configuration  */
+  GPIO_InitStruct.Pin       = USARTx_TX_PIN;
+  GPIO_InitStruct.Mode      = GPIO_MODE_AF_PP;
+  GPIO_InitStruct.Pull      = GPIO_PULLUP;
+  GPIO_InitStruct.Speed     = GPIO_SPEED_FREQ_HIGH;
+
+  HAL_GPIO_Init(USARTx_TX_GPIO_PORT, &GPIO_InitStruct);
+
+  /* UART RX GPIO pin configuration  */
+  GPIO_InitStruct.Pin = USARTx_RX_PIN;
+  GPIO_InitStruct.Mode      = GPIO_MODE_INPUT;
+
+  HAL_GPIO_Init(USARTx_RX_GPIO_PORT, &GPIO_InitStruct);
+
+  /*##-3- Configure the DMA ##################################################*/
+  /* Configure the DMA handler for Transmission process */
+  hdma_tx.Instance                 = USARTx_TX_DMA_CHANNEL;
+  hdma_tx.Init.Direction           = DMA_MEMORY_TO_PERIPH;
+  hdma_tx.Init.PeriphInc           = DMA_PINC_DISABLE;
+  hdma_tx.Init.MemInc              = DMA_MINC_ENABLE;
+  hdma_tx.Init.PeriphDataAlignment = DMA_PDATAALIGN_BYTE;
+  hdma_tx.Init.MemDataAlignment    = DMA_MDATAALIGN_BYTE;
+  hdma_tx.Init.Mode                = DMA_NORMAL;
+  hdma_tx.Init.Priority            = DMA_PRIORITY_LOW;
+
+  HAL_DMA_Init(&hdma_tx);
+
+  /* Associate the initialized DMA handle to the UART handle */
+  __HAL_LINKDMA(huart, hdmatx, hdma_tx);
+
+  /* Configure the DMA handler for reception process */
+  hdma_rx.Instance                 = USARTx_RX_DMA_CHANNEL;
+  hdma_rx.Init.Direction           = DMA_PERIPH_TO_MEMORY;
+  hdma_rx.Init.PeriphInc           = DMA_PINC_DISABLE;
+  hdma_rx.Init.MemInc              = DMA_MINC_ENABLE;
+  hdma_rx.Init.PeriphDataAlignment = DMA_PDATAALIGN_BYTE;
+  hdma_rx.Init.MemDataAlignment    = DMA_MDATAALIGN_BYTE;
+  hdma_rx.Init.Mode                = DMA_NORMAL;
+  hdma_rx.Init.Priority            = DMA_PRIORITY_HIGH;
+
+  HAL_DMA_Init(&hdma_rx);
+
+  /* Associate the initialized DMA handle to the the UART handle */
+  __HAL_LINKDMA(huart, hdmarx, hdma_rx);
+
+  /*##-4- Configure the NVIC for DMA #########################################*/
+  /* NVIC configuration for DMA transfer complete interrupt (USARTx_TX) */
+  HAL_NVIC_SetPriority(USARTx_DMA_TX_IRQn, 0, 1);
+  HAL_NVIC_EnableIRQ(USARTx_DMA_TX_IRQn);
+
+  /* NVIC configuration for DMA transfer complete interrupt (USARTx_RX) */
+  HAL_NVIC_SetPriority(USARTx_DMA_RX_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(USARTx_DMA_RX_IRQn);
+
+  /* NVIC configuration for USART, to catch the TX complete */
+  HAL_NVIC_SetPriority(USARTx_IRQn, 0, 1);
+  HAL_NVIC_EnableIRQ(USARTx_IRQn);
+}
+
+
+#if 0
+/**
+  * @brief UART MSP De-Initialization
+  *        This function frees the hardware resources used in this example:
+  *          - Disable the Peripheral's clock
+  *          - Revert GPIO, DMA and NVIC configuration to their default state
+  * @param huart: UART handle pointer
+  * @retval None
+  */
+void HAL_UART_MspDeInit(UART_HandleTypeDef *huart)
+{
+
+  static DMA_HandleTypeDef hdma_tx;
+  static DMA_HandleTypeDef hdma_rx;
+
+  /*##-1- Reset peripherals ##################################################*/
+  USARTx_FORCE_RESET();
+  USARTx_RELEASE_RESET();
+
+  /*##-2- Disable peripherals and GPIO Clocks ################################*/
+  /* Configure UART Tx as alternate function  */
+  HAL_GPIO_DeInit(USARTx_TX_GPIO_PORT, USARTx_TX_PIN);
+  /* Configure UART Rx as alternate function  */
+  HAL_GPIO_DeInit(USARTx_RX_GPIO_PORT, USARTx_RX_PIN);
+  
+  /*##-3- Disable the DMA Channels ###########################################*/
+  /* De-Initialize the DMA Channel associated to transmission process */
+  HAL_DMA_DeInit(&hdma_tx);
+  /* De-Initialize the DMA Channel associated to reception process */
+  HAL_DMA_DeInit(&hdma_rx);
+
+  /*##-4- Disable the NVIC for DMA ###########################################*/
+  HAL_NVIC_DisableIRQ(USARTx_DMA_TX_IRQn);
+  HAL_NVIC_DisableIRQ(USARTx_DMA_RX_IRQn);
+}
+#endif
 
 
 #ifdef __cplusplus
